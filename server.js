5ea72e84aae36720dccd16a4dafc01540a4a19b5
apiet <47141162+apietdoes6@users.noreply.github.com>
2020-03-15 05:50:00 +0700

    sdsdsdasd

--- server.js
+++ server.js
@@ -6,41 +6,14 @@ const helmet = require('helmet')
 const road = require('./src/routes/routes')
 const sendMail = require('./mail')
 const path = require('path');
-const log  = console.log;
+
 
 const dbRoute = "mongodb+srv://padarofficial:padar2902@padar-npjua.mongodb.net/padar_animation?retryWrites=true&w=majority";
 
 
 //EXPRESS INIT
 const app = express();
-const PORT = 5000;
-
-
-// Email sent from kontak.js
-// Data parsing
-app.use(express.urlencoded({
-    extended: false
-}));
-app.use(express.json());
-
-// email, name, text
-app.post('/email', (req, res) => {
-    const { name, email, text } = req.body;
-    console.log('Data: ', req.body);
-
-    sendMail( email, name, text, function(err, data) {
-        if (err) {
-            res.status(500).json({ message: 'Internal Error' });
-        } else {
-            res.json({ message: 'Email sent!!!' })
-        }
-    });
-});
-
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'pages', 'Kontak.js'));
-});
+const PORT = process.env.PORT || 5000;
 
 
 
